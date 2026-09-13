a690e2bfd131611605fa606ad4f8a84d4450bd0d
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-13 17:14:55 +0900

    feat: learn bollean in python

--- primitive.py
+++ primitive.py
@@ -19,7 +19,7 @@ result = type(course)
 print(f"the result (1):{result}")
 
 result = course.title()
-print("the result (2): {result}")
+print(f"the result (2): {result}")
 
 result = course.upper()
 print(f"the result (3): {result}")
@@ -28,4 +28,21 @@ result = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result}")
 print(course)
 
+
+
 print("====== boolean ======")
+# functions > type() input()  bool() int() str()
+y = input("Give your value for y:")
+print("y",y)
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# Truthy vs Falsy value
+# Truthy : true 100 -100 "MIT"
+# Falsy : false 0 "" NONE
+
+test_falsy = "" or False or None or 0 or 100
+print("The FALSY:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("The TRUTHY:", bool(test_truthy))
