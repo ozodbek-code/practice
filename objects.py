3255a6be3e8fd9461ce68f26ace619948288f4b8
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-16 10:31:04 +0900

    feat: Learn pyhton objects

---
+++ objects.py
@@ -0,0 +1,23 @@
+import array # package/module
+import math # package
+from math import ceil, asin
+
+print("======== What is object ========")
+
+# An object has state, methods, and properties.
+# Everything is an object in Python
+
+print(type("Hello World"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigms > Functional Programming & OOP
+# OOP 4 concepts > Abstraction | Encapsulation | Inheritance | Polymorphism
+
+result1 = math.ceil(97.7)
+print("result1:", result1)
+
+result2 = ceil(98.7)
+print("result2:", result2)
